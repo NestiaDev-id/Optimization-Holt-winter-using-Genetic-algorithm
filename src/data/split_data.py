ede3a3e98cbad99838518f10473461ed325f9a84
NestiaDev-id <yohanesdevano90@gmail.com>
2025-04-23 11:29:45 +0700

    update requirements

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def time_series_split(df, date_column='tanggal', split_date='2022-01-01'):
+def time_series_split(df):
     """
     Membagi data ke dalam train dan test.
     """
     
     # Bagi data 90 persent ke train dan 10 persent ke test
-    train = df['jumlah'][12:].copy()
-    test = df.tail(12).copy()
+    train = df[['jumlah']][:204]
+    test = df[['jumlah']][204:216]
     
     return train, test
