ddc3e7bec574b77bf589cc3d489a04a0cf273ba4
NestiaDev-id <yohanesdevano90@gmail.com>
2025-04-28 05:08:16 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import endpoints
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
 
 app = FastAPI(
     title="My ML API",
     description="Backend untuk ML model serving",
     version="0.1.0",
+)
+
+# Get FRONTEND_URL from environment
+frontend_url = os.getenv("FRONTEND_URL")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontend_url] if frontend_url else ["*"],  # Allow specific frontend URL or all origins
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
 )
 
 app.include_router(endpoints.router, prefix="/api")
@@ -15,6 +29,3 @@ app.include_router(endpoints.router, prefix="/api")
 def read_root():
     return {"message": "Welcome to My ML API!"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
